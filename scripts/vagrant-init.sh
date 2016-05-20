5007890886e874d37647b1f08364ad8bb9eabb89
Joe Stewart <joe.stewart@mediacurrent.com>
2016-05-20 15:57:44 -0500

    MCT-12 vagrant is now installed via composer.

--- scripts/vagrant-init.sh
+++ scripts/vagrant-init.sh
@@ -6,17 +6,13 @@ if [ ! -z $1 ]; then
   BASE_DIR=$1
 fi
 
-if [[ ! -d ${BASE_DIR}/box ]]; then
-  git clone -b feature/MCT-12-subdirectory git@bitbucket.org:mediacurrent/mis_vagrant.git ${BASE_DIR}/box
-fi
-
-if [[ ! -f ${BASE_DIR}/config/drupal-vm.config.yml ]]; then
+if [[ ! -f ${BASE_DIR}/config/config.yml ]]; then
   if [[ ! -d ${BASE_DIR}/config ]]; then
     mkdir ${BASE_DIR}/config
   fi
-  cp ${BASE_DIR}/box/example.config.yml ${BASE_DIR}/config/drupal-vm.config.yml
+  cp ${SCRIPT_DIR}/../files/example.config.yml ${BASE_DIR}/config/config.yml
 fi
 
 if [[ ! -f ${BASE_DIR}/Vagrantfile ]]; then
-  cp ${BASE_DIR}/box/Vagrantfile.parent ${BASE_DIR}/Vagrantfile
+  cp ${SCRIPT_DIR}/../files/Vagrantfile.parent ${BASE_DIR}/Vagrantfile
 fi
