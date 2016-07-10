12529bee55cc27d98f7fa97214ba658d75d3d158
Joe Stewart <joe.stewart@mediacurrent.com>
2016-07-10 16:35:37 -0500

    MCT-73 robo console help text

--- src/Command/Console.php
+++ src/Command/Console.php
@@ -8,6 +8,9 @@ trait Console
     /**
      * Drupal Console command.
      *
+     * Wrapper for Drupal Console that adds the uri and root arguments.
+     * The uri and root arguments are calculated from the project directory
+     * and the config.yml file.
      *
      * @return object Result
      */
