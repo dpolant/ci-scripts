81f78a1b55b2a3dd6512b23a436ba5acdc52167e
Joe Stewart <joe.stewart@mediacurrent.com>
2016-07-10 10:22:31 -0500

    MCT-73 Add console options handling
    Added initial console test

--- src/Command/Console.php
+++ src/Command/Console.php
@@ -8,13 +8,15 @@ trait Console
     /**
      * Drupal Console command.
      *
+     *
      * @return object Result
      */
-    public function console($console_command = null)
+    public function console($console_command = null, $opts = null)
     {
 
         $this->taskConsole()
             ->consoleCommand($console_command)
+            ->consoleOptions($opts)
             ->run();
     }
 
--- src/Task/Console.php
+++ src/Task/Console.php
@@ -2,17 +2,25 @@
 
 namespace Mediacurrent\CiScripts\Task;
 
-
 use Robo\Result;
+use Robo\Exception\TaskException;
 
 class Console extends \Mediacurrent\CiScripts\Task\Base
 {
+    use \Robo\Task\Base\loadTasks;
     use \Robo\Task\Remote\loadTasks;
 
     protected $arg;
     protected $console_command;
+    protected $console_options;
     protected $root;
     protected $uri;
+
+    public function arg($arg = null) {
+        $this->arg = $arg;
+
+        return $this;
+    }
 
     public function consoleCommand($console_command = null) {
         $this->console_command = $console_command;
@@ -20,22 +28,42 @@ class Console extends \Mediacurrent\CiScripts\Task\Base
         return $this;
     }
 
-    public function arg($arg = null) {
-        $this->arg = $arg;
+    public function consoleOptions($console_options = null) {
+        $this->console_options = $console_options;
 
         return $this;
     }
- 
+
+    public function getCommand($pathToInstallDir = null, $uri = null) {
+        
+        if (!$pathToInstallDir) {
+             $pathToInstallDir = $this->configuration['drupal_composer_install_dir'];
+        }
+
+        $console = $pathToInstallDir . '/bin/drupal';
+        $root = $pathToInstallDir . '/web';
+        if(!$uri) {
+            $uri = 'http://' . $this->configuration['vagrant_hostname'];
+        }
+        $command = $console . ' --uri=' . $uri . ' --root=' . $root . ' ' . $this->console_command; 
+        if($this->arg) {
+            $command .= ' ' . $this->arg;
+        }
+        if($this->console_options) {
+            $command .= ' ' . $this->console_options;
+        }
+
+        return $command;
+    }
+
     /**
      * @return Result
      */
     public function run()
     {
-        $console = $this->configuration['drupal_composer_install_dir'] . '/bin/drupal';
-        $root = $this->configuration['drupal_composer_install_dir'] . '/web';
-        $uri = 'htp://' . $this->configuration['vagrant_hostname'];
-        $command = $console . ' --uri=' . $uri . ' --root=' . $root . ' ' . $this->console_command . ' ' . $this->arg;
         
+        $command = $this->getCommand();
+
         $this->printTaskInfo($command);
         if($this->useVagrant()) {
             $this->taskSshExec($this->configuration['vagrant_hostname'], 'vagrant')
